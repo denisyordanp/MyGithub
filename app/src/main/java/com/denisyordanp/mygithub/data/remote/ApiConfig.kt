133a3e89d735ffaee3649b6c3dd612449ac79ffe
Denis Yordan <denis.yordan.p@gmail.com>
2021-10-14 16:10:01 +0700

    add header

--- app/src/main/java/com/denisyordanp/mygithub/data/remote/ApiConfig.kt
+++ app/src/main/java/com/denisyordanp/mygithub/data/remote/ApiConfig.kt
@@ -1,24 +1,39 @@
 package com.denisyordanp.mygithub.data.remote
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
-    companion object{
+    companion object {
+
+        private const val BASE_URL = "https://api.github.com/"
+        private const val API_KEY = "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"
+
         fun getApiService(): GithubService {
-            val loggingInterceptor =
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .build()
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
-                .build()
-            return retrofit.create(GithubService::class.java)
+                .build().create(GithubService::class.java)
         }
+
+        private val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        private val header = Interceptor { chain ->
+            val newRequest = chain.request().newBuilder()
+                .addHeader("Authorization", API_KEY)
+                .build()
+
+            return@Interceptor chain.proceed(newRequest)
+        }
+
+        private val client = OkHttpClient.Builder()
+            .addInterceptor(header)
+            .addInterceptor(loggingInterceptor)
+            .build()
     }
 }
